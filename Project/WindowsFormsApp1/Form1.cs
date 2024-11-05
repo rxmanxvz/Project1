10a30328d765e76ba303c7b907d91559460bb011
rxmanxvz <relbarkano@gmail.com>
2024-11-05 19:56:32 +0300

    Добавление программы

---
+++ Project/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Random random;
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Подбросить монету";
+            label1.Text = "";
+            button1.Text = "Подбросить монету";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int storonaMonety = random.Next(0, 2);
+            if (storonaMonety == 0)
+            {
+                label1.Text = "Орёл";
+            }
+            else
+            {
+                label1.Text = "Решка";
+            }
+        }
+    }
+}
